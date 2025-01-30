c521fc8c3ddab8626d0bcdffea149e569ad438c3
Casper Andresen <casper.andresen@cardlay.com>
2025-01-30 13:29:16 +0100

    remove redundant field

--- models/src/main/kotlin/com/example/Request.kt
+++ models/src/main/kotlin/com/example/Request.kt
@@ -1,10 +1,7 @@
 package com.example
 
-import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
 data class Request(
     val list: List<@NotNull String>,
-    @field:NotBlank
-    val string: String
 )
